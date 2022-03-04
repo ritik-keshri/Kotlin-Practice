c00ea04995d6c87c87e22472bcca6245f50e6a1f
Ritik <ritik.k@redjinni.com>
2022-03-04 10:50:05 +0530

    Dynamic view data retrival code optimmization is Added

--- app/src/main/java/com/example/myapplication/DynamicViewAdding/DynamicViewAdding.kt
+++ app/src/main/java/com/example/myapplication/DynamicViewAdding/DynamicViewAdding.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.DynamicViewAdding
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
@@ -10,10 +11,14 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 import kotlinx.android.synthetic.main.activity_dynamic_view_adding.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DynamicViewAdding : AppCompatActivity() {
+
     lateinit var layout: LinearLayout
     var languageList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dynamic_view_adding)
@@ -23,17 +28,28 @@ class DynamicViewAdding : AppCompatActivity() {
         addView.setOnClickListener {
             addView()
         }
+
         showData.setOnClickListener {
-            savedata()
+//            savedata()
             showDa()
+//            languageList.clear()
         }
     }
 
     private fun showDa() {
+//        val count = linearLayout.childCount
+//        for (i in 0 until count) {
+//            Toast.makeText(this, "Language at $i is $languageList[i] ", Toast.LENGTH_SHORT).show()
+//        }
+        languageList.add(0,txt.text.toString())
         val count = linearLayout.childCount
+        var v: View?
         for (i in 0 until count) {
-            Toast.makeText(this, "Language at $i is $languageList[i] ", Toast.LENGTH_SHORT).show()
+            v = linearLayout.getChildAt(i)
+            val languageName: EditText = v.findViewById(R.id.na)
+            languageList.add(languageName.text.toString())
         }
+        Log.d("TAG", "showDa: $languageList")
     }
 
     private fun addView() {
@@ -41,19 +57,21 @@ class DynamicViewAdding : AppCompatActivity() {
         val view: View = inflater.inflate(R.layout.dynamic_view_layout, null)
         layout.addView(view)
 
-        val languageName: EditText = view.findViewById(R.id.na)
-        languageList.add(languageName.text.toString())
+
+//        var v=linearLayout.getChildAt(int)
+//        val languageName: EditText = v.findViewById(R.id.na)
+//        languageList.add(languageName.text.toString())
+//        Log.d("TAG", "Add: $int "+languageName.text.toString())
     }
 
-    fun savedata() {
-        val count = linearLayout.childCount
-        var v: View?
-        for (i in 0 until count) {
-            v = linearLayout.getChildAt(i)
-            val languageName: EditText = v.findViewById(R.id.na)
-            // add the data to arraylist
-            languageList.add(languageName.text.toString())
-        }
-
-    }
+//    fun savedata() {
+//        val count = linearLayout.childCount
+//        var v: View?
+//        for (i in 0 until count) {
+//            v = linearLayout.getChildAt(i)
+//            val languageName: EditText = v.findViewById(R.id.na)
+//            // add the data to arraylist
+//            languageList.add(languageName.text.toString())
+//        }
+//    }
 }
